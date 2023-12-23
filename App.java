79054f4336c1a4e4ebeabee12577e2f878fd38f0
Elizar Konovalov <conovalov.elizar@gmail.com>
2023-12-24 03:52:22 +0600

    Implemented methods for finding the most expensive and cheapest home orders

--- App.java
+++ App.java
@@ -2,6 +2,7 @@ import domain.Order;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class App {
@@ -17,9 +18,15 @@ public class App {
 //        System.out.println("------ Самые маленькие заказы -----");
 //        getTheSmallestOrders(5).forEach(System.out::println);
 //        System.out.println("-----------------------------------");
-        System.out.println("------ Заказы с доставкой на дом -----");
-        getDeliveryOrders().forEach(System.out::println);
-        System.out.println(getDeliveryOrders().size());
+//        System.out.println("------ Заказы с доставкой на дом -----");
+//        getDeliveryOrders().forEach(System.out::println);
+//        System.out.println(getDeliveryOrders().size());
+//        System.out.println("--------------------------------------");
+        System.out.println("--  Самый дорогой заказ на дом  --");
+        getDeliveryTopOrder().forEach(System.out::println);
+        System.out.println("--------------------------------------");
+        System.out.println("- Самый маленький заказ на дом -");
+        getDeliverySmallestOrder().forEach(System.out::println);
         System.out.println("--------------------------------------");
     }
 
@@ -41,6 +48,18 @@ public class App {
         return restarauntOrders.stream()
                 .filter(Order::isHomeDelivery)
                 .collect(Collectors.toList());
+    }
+
+    public List<Order> getDeliveryTopOrder() {
+        return getDeliveryOrders().stream()
+                .max(Comparator.comparingDouble(Order::getTotal))
+                .stream().collect(Collectors.toList());
+    }
+
+    public List<Order> getDeliverySmallestOrder() {
+        return getDeliveryOrders().stream()
+                .min(Comparator.comparingDouble(Order::getTotal))
+                .stream().collect(Collectors.toList());
     }
 
 }
